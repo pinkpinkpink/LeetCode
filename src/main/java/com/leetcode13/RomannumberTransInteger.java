1c18d261d86c1b8f0b601cedc80f5757f21ce600
wangyifan <15638515778@163.com>
2018-08-27 22:29:29 +0800

    RomanNumberTransInteger

--- src/main/java/com/leetcode13/RomannumberTransInteger.java
+++ src/main/java/com/leetcode13/RomannumberTransInteger.java
@@ -6,25 +6,27 @@ package com.leetcode13;/*
  * */
 
 public class RomannumberTransInteger {
-    public int romanToInt(String r){
-        String I;
-        String V;
-        String X;
-        String L;
-        String C;
-        String D;
-        String M;
-        String ADD;
-        String NoADD;
-        private String left;
-        private String right;
+    public int romanToInt(char r){
+
+        switch (r){
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+        }
+        return 0;
 
 
-    if(){
-
-    }else{
-
-    }
 
     }
 
